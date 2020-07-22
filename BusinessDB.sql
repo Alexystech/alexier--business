b36749866774d952d0fa0d69e7e553879fd97e80
Alexis Vazquez <62068384+Alexystech@users.noreply.github.com>
2020-07-22 13:58:28 -0500

    mas script de base de datos

--- BusinessDB.sql
+++ BusinessDB.sql
@@ -1,95 +1,97 @@
-create table empresa
+create schema if not exists negocio collate utf8mb4_0900_ai_ci;
+
+use negocio;
+
+create table if not exists empresa
 (
-    codigo int         not null
-        primary key,
-    nombre varchar(30) null
+	codigo int not null
+		primary key,
+	nombre varchar(30) null
 );
 
-create table tipo_producto
+create table if not exists tipo_producto
 (
-    dni                  int         not null
-        primary key,
-    nombre_tipo_producto varchar(30) null
+	dni int not null
+		primary key,
+	nombre_tipo_producto varchar(30) null
 );
 
-create table bebida_alcoholica
+create table if not exists bebida_alcoholica
 (
-    codigo_bebida int         not null
-        primary key,
-    dni           int         null,
-    nombre_bebida varchar(30) null,
-    precio        double      null,
-    constraint bebida_alcoholica_ibfk_1
-        foreign key (dni) references tipo_producto (dni)
-);
-
-create index dni
-    on bebida_alcoholica (dni);
-
-create table bebida_energetica
-(
-    codigo_bebida int         not null
-        primary key,
-    dni           int         null,
-    nombre_bebida varchar(30) null,
-    precio        double      null,
-    constraint bebida_energetica_ibfk_1
-        foreign key (dni) references tipo_producto (dni)
+	codigo_bebida int not null
+		primary key,
+	dni int null,
+	nombre_bebida varchar(30) null,
+	precio double null,
+	constraint bebida_alcoholica_ibfk_1
+		foreign key (dni) references tipo_producto (dni)
 );
 
 create index dni
-    on bebida_energetica (dni);
+	on bebida_alcoholica (dni);
 
-create table bebida_soda
+create table if not exists bebida_energetica
 (
-    codigo_bebida int         not null
-        primary key,
-    dni           int         null,
-    nombre_bebida varchar(30) null,
-    precio        double      null,
-    constraint bebida_soda_ibfk_1
-        foreign key (dni) references tipo_producto (dni)
+	codigo_bebida int not null
+		primary key,
+	dni int null,
+	nombre_bebida varchar(30) null,
+	precio double null,
+	constraint bebida_energetica_ibfk_1
+		foreign key (dni) references tipo_producto (dni)
 );
 
 create index dni
-    on bebida_soda (dni);
+	on bebida_energetica (dni);
 
-create table galletas
+create table if not exists bebida_soda
 (
-    codigo_galletas int         not null
-        primary key,
-    dni             int         null,
-    nombre_galletas varchar(30) null,
-    precio          double      null,
-    constraint galletas_ibfk_1
-        foreign key (dni) references tipo_producto (dni)
+	codigo_bebida int not null
+		primary key,
+	dni int null,
+	nombre_bebida varchar(30) null,
+	precio double null,
+	constraint bebida_soda_ibfk_1
+		foreign key (dni) references tipo_producto (dni)
 );
 
 create index dni
-    on galletas (dni);
+	on bebida_soda (dni);
 
-create table lacteos
+create table if not exists galletas
 (
-    codigo_lacteo int         not null
-        primary key,
-    dni           int         null,
-    nombre_lacteo varchar(30) null,
-    precio        double      null,
-    constraint lacteos_ibfk_1
-        foreign key (dni) references tipo_producto (dni)
+	codigo_galletas int not null
+		primary key,
+	dni int null,
+	nombre_galletas varchar(30) null,
+	precio double null,
+	constraint galletas_ibfk_1
+		foreign key (dni) references tipo_producto (dni)
 );
 
 create index dni
-    on lacteos (dni);
+	on galletas (dni);
 
-create table ventas
+create table if not exists lacteos
 (
-    codigo        int         not null
-        primary key,
-    empresa       varchar(30) null,
-    tipo_producto varchar(30) null,
-    producto      varchar(30) null,
-    precio        double      null
+	codigo_lacteo int not null
+		primary key,
+	dni int null,
+	nombre_lacteo varchar(30) null,
+	precio double null,
+	constraint lacteos_ibfk_1
+		foreign key (dni) references tipo_producto (dni)
 );
 
+create index dni
+	on lacteos (dni);
 
+create table if not exists ventas
+(
+	codigo int not null
+		primary key,
+	empresa varchar(30) null,
+	tipo_producto varchar(30) null,
+	producto varchar(30) null,
+	precio double null
+);
